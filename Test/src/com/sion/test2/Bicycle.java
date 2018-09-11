776d55bc593f63c76add6cf4d021195b302931c9
윤시온 <sion.yun@tlx.co.kr>
2018-09-11 13:53:42 +0900

    초기 세팅

---
+++ Test/src/com/sion/test2/Bicycle.java
@@ -0,0 +1,20 @@
+package com.sion.test2;
+
+public class Bicycle {
+
+	public String color;
+	public int price;
+
+	public Bicycle() {
+		System.out.println("Bicycle constructor");
+	}
+	
+	public void info() {
+		System.out.println("-- info() --");
+		System.out.println("color : " + color);
+		System.out.println("price : " + price);
+	}
+	
+	
+	
+}
